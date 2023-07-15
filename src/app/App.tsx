1f4eb8cdfbb580577ef58a5c0faa7a9c7422836b
desertfoox <salar.nili097@gmail.com>
2023-07-16 01:44:03 +0330

    add task management

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,54 @@
+import TaskCard from "../components/TaskCard/TaskCard";
 
-function App() {
+export default function App() {
+  const task = {
+    title: "Create Task Managment",
+    creator: "Salar",
+    tag: "Project",
+    description:
+      "Task Description Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus autem illum repudiandae recusandae maxime debitis quod rem et veniam, possimus libero quaerat optio, eaque dolorem ex cupiditate accusantium, sunt aspernatur.",
+  };
 
   return (
-    <>
-     <h1>salam</h1>
-    </>
-  )
-}
+    <div className="mx-auto bg-[#1C1A42] w-[1300px]  h-a  rounded-md mt-10 p-3">
+      <h1 className="text-center text-white text-3xl font-bold mt-3">
+        Todolist
+      </h1>
 
-export default App
+      <div className="flex justify-between mt-6">
+        <div className="w-1/4">
+          <h2 className="text-center bg-red-500 rounded-lg  bg-opacity-75 p-4 text-red-100 text-lg font-semibold mb-2">
+            Not Started
+          </h2>
+          <TaskCard
+            title={task.title}
+            creator={task.creator}
+            tag={task.tag}
+            description={task.description}
+            startDate={"1402/2/3"}
+            endDate={"1402/2/3"}
+          />  
+        
+        </div>
+        <div className="w-1/4 ">
+          <h2 className="text-center text-yellow-100 text-lg font-semibold mb-2 bg-yellow-500 bg-opacity-75 rounded-lg p-4 ml-4">
+            In Progress
+          </h2>
+          {/* In progress tasks */}
+        </div>
+        <div className="w-1/4">
+          <h2 className="text-center text-green-100 text-lg font-semibold mb-2  bg-green-500 bg-opacity-75 rounded-lg p-4 ml-4">
+            Completed
+          </h2>
+          {/* Completed tasks */}
+        </div>
+        <div className="w-1/4 ">
+          <h2 className="text-center text-blue-100 text-lg font-semibold mb-2 bg-blue-500 bg-opacity-75 rounded-lg p-4 ml-4">
+            Testing
+          </h2>
+          {/* Testing tasks */}
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/TaskCard/TaskCard.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+interface TaskCardProps {
+  title: string;
+  creator: string;
+  tag: string;
+  description: string;
+  startDate: string;
+  endDate: string;
+}
+
+const TaskCard: React.FC<TaskCardProps> = ({ title, creator, tag, description, startDate, endDate }) => {
+  const truncateDescription = (description: string, wordLimit: number): string => {
+    const words = description.split(' ');
+    if (words.length > wordLimit) {
+      return words.slice(0, wordLimit).join(' ') + '...';
+    }
+    return description;
+  };
+
+  return (
+    <div className="bg-white rounded-lg p-4 shadow-md mb-2">
+      <h3 className="text-lg font-semibold mb-2">{title}</h3>
+      <p className="text-[#9B7B8A] text-sm mb-2 bg-[#F5E0E9] w-[5rem] text-center rounded px-2">{tag}</p>
+      <p className="text-gray-500 text-sm mb-2 px-2">Creator: {creator}</p>
+      <p className="text-gray-500 text-sm mb-2 px-2">{startDate} {'--->'} {endDate}</p>
+      <p className="text-gray-700 text-sm px-2">
+        {truncateDescription(description, 7)}
+      </p>
+    </div>
+  );
+};
+
+export default TaskCard;
